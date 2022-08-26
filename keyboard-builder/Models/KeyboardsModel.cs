252fb6207d477d93497300752a23ea2ba4f91a29
albert <albertnt905@gmail.com>
2022-08-25 23:55:45 -0400

    -

---
+++ keyboard-builder/Models/KeyboardsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace keyboard_builder.Models
+{
+    public class KeyboardsModel
+    {
+        public string ImageTitle { get; set; }
+        public string KeyboardName { get; set; }
+        public float BasePrice { get; set; } = 200;
+        public bool Switches { get; set; }
+        public bool Keycaps { get; set; }
+        public float FinalPrice { get; set; }
+    }
+}
+
