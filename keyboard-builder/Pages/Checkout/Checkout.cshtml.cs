37c26e20c0d673985cd834fe8ff9e78df80168f6
albert <albertnt905@gmail.com>
2022-08-31 02:56:33 -0400

    -

---
+++ keyboard-builder/Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace keyboard_builder.Pages.Checkout
+{
+    [BindProperties(SupportsGet = true)]
+
+    public class CheckoutModel : PageModel
+    {
+        public string KeyboardName { get; set; }
+        public float KeyboardPrice { get; set; }
+        public string ImageTitle { get; set; }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(KeyboardName))
+            {
+                KeyboardName = "Custom";
+            }
+
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+        }
+    }
+}
--- keyboard-builder/Pages/Index.cshtml.cs
+++ keyboard-builder/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using keyboard_builder.Models;
 
 namespace keyboard_builder.Pages;
 
@@ -11,6 +12,67 @@ public class IndexModel : PageModel
     {
         _logger = logger;
     }
+
+    public List<KeyboardsModel> fakeKeyboardDb = new List<KeyboardsModel>()
+    {
+        new KeyboardsModel(){
+            ImageTitle="LeopoldFC900R",
+            KeyboardName="LeopoldFC900R",
+            BasePrice=100,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=150
+        },
+        new KeyboardsModel(){
+            ImageTitle="DuckyOne3",
+            KeyboardName="DuckyOne3",
+            BasePrice=110,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=160
+        },
+        new KeyboardsModel(){
+            ImageTitle="ObinslabAnne",
+            KeyboardName="ObinslabAnne",
+            BasePrice=120,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=170
+        },
+        new KeyboardsModel(){
+            ImageTitle="VarmilloVA87M",
+            KeyboardName="VarmilloVA87M",
+            BasePrice=130,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=180
+        },
+        new KeyboardsModel(){
+            ImageTitle="VortexCore",
+            KeyboardName="VortexCore",
+            BasePrice=140,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=190
+        },
+        new KeyboardsModel(){
+            ImageTitle="LeopoldFC660C",
+            KeyboardName="LeopoldFC660",
+            BasePrice=150,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=200
+        },
+        new KeyboardsModel(){
+            ImageTitle="VortexRace3",
+            KeyboardName="VortexRace3",
+            BasePrice=160,
+            Keycaps=true,
+            Switches=true,
+            FinalPrice=210
+        },
+
+    };
 
     public void OnGet()
     {
